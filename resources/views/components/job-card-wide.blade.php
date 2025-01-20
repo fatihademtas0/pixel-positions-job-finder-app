7c6e5217f046801b94c9c94501497aadd32234d2
fatihademtas0 <fatihademtas@outlook.com>
2025-01-20 18:36:29 +0300

    first commit

---
+++ resources/views/components/job-card-wide.blade.php
@@ -0,0 +1,23 @@
+<x-panel class="flex gap-x-6">
+    <div>
+        <x-employer-logo/>
+    </div>
+
+
+    <div class="flex-1 flex flex-col">
+
+    <a href="#" class="self-start text-sm text-gray-400">Laracast</a>
+
+        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-800 transition-colors duration-300">Video Producer</h3>
+        <p class="text-sm text-gray-400 mt-auto">Full Time - From $60,000</p>
+
+    </div>
+
+    <div class="space-x-1 flex items-center">
+
+        <x-tag>Frontend</x-tag>
+        <x-tag>Backend</x-tag>
+        <x-tag>API</x-tag>
+
+    </div>
+</x-panel>
